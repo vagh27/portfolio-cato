69010f35c60d640178a6a5a849f5861fd3578471
catob <cato.benjaminsen@gmail.com>
2014-11-14 00:20:33 -0500

    first commit

---
+++ js/base.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	$('.about-buttons a').click(function(){
+
+	var text = $(this).data('text'),
+		title = $(this).data('title'),
+		url = $(this).data('url');
+
+	$('.dynamicText').html(text);
+	$('.dynamicTitle').html(title);
+	$('.dynamicURL').attr('href',url).text("Link >");
+
+});
+
+	});
