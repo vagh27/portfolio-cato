ff76bf731f2109cf1d23ffd3637f93ab110464b3
catob <cbenjaminsen@Catos-MacBook-Air-5.local>
2014-11-17 18:17:26 -0500

    added js

--- js/base.js
+++ js/base.js
@@ -1,18 +1,21 @@
 $(function() {
-  // get nav height
-  var navHeight = $('.top-navigation').height();
+	// get nav height
+	var navHeight = $('.top-navigation').height();
 
-  // smooth scroll anchor links
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top-navHeight
-        }, 1000);
-        return false;
-      }
-    }
-  });
+	// smooth scroll anchor links
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	    	var target = $(this.hash);
+	    	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    	if (target.length) {
+	      		$('html,body').animate({
+	        	scrollTop: target.offset().top-navHeight
+	      		}, 1000);
+	      		return false;
+	    	}
+		}
+	});
+  	$('.mobileMenu').click(function(){ // THIS SHOWS THE MOBILE NAVI //
+  		$('.nav').toggleClass('navShow');
+	});
 });
